2c898de4fb6f6f7688e0fc6986c9997afd4244ce
cradle0fFilth <3175806342@qq.com>
2021-10-31 11:31:49 -0400

    add python file

---
+++ query-price-bsc.py
@@ -0,0 +1,38 @@
+from web3 import Web3
+import json
+import requests
+import math
+
+# add your blockchain connection information
+chain = input("What chain you want to query(eth or bsc or polygon)?: ")
+url = 'https://speedy-nodes-nyc.moralis.io/{api-key}/' + chain + '/mainnet'
+web3 = Web3(Web3.HTTPProvider(url))
+print(web3.isConnected())
+
+
+token_address = web3.toChecksumAddress(input("Enter TokenAddress: "))
+
+token_balance = 'https://deep-index.moralis.io/api/v2/erc20/'+ token_address + '/price?chain=' + chain
+
+erc20_abi='[ { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ]'
+
+contract = web3.eth.contract(address = token_address,abi = erc20_abi)
+
+name = contract.functions.name().call()
+symbol = contract.functions.symbol().call()
+
+headers = {
+'x-api-key':'private-key'}
+
+response = requests.request("GET", token_balance, headers=headers)
+resp = response.json()
+print(resp)
+
+price = resp['nativePrice']['value']
+priceBNB = web3.fromWei(int(price),'ether')
+priceUSD = resp['usdPrice']
+
+print(f"The price of {symbol} is: ")
+print("{:.9f}".format(priceBNB) + " BNB")
+print("{:.2f}".format(priceUSD) + " USD")
+
